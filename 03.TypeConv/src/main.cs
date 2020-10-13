bdf158a3c83575e2936b82122f2768846e65105c
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-13 17:04:57 +0530

    Types

---
+++ 03.TypeConv/src/main.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class main {
+	public static void Main() {
+		 
+    int myInt = 10;
+	double myDouble = 5.25;
+	bool myBool = true;
+
+	Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+	Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+	Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+	Console.WriteLine(Convert.ToString(myBool));   // convert bool to stri
+	}
+}
