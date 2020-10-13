2f5760cd27660a66d56fb0e969104cb936f1e675
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-13 07:35:05 +0530

    Var intiger

---
+++ 02.Var_Int/src/main.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class main {
+	public static void Main() {
+		 
+         int a ;
+         double b,c;
+         a = 10;
+         b = 20;
+         c = a + b;
+         Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+         Console.ReadLine();
+	}
+}
