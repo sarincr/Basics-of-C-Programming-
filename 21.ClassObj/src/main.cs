9ed441a54579653d0b9e9d8af20b60e4fd719fe4
sarin cr <sarinrays@gmail.com>
2021-04-11 12:12:39 +0530

    Class Obj

---
+++ 21.ClassObj/src/main.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace NewClass
+{
+  class Name
+  {
+    string data = "John";
+
+    static void Main(string[] args)
+    {
+      Name myObj = new Name();
+      Console.WriteLine(myObj.data);
+    }
+  }
+}
